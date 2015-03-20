47142cbee51c0b2f05b81c7170d41fb767fdd790
Phillip <phv9495@saintjoe.edu>
2015-03-19 23:42:36 -0400

    Added 0's to the single digits so that the dates are more easily parsed.

--- buildings.js
+++ buildings.js
@@ -1,12 +1,12 @@
 var BUILDINGS = [
-  {name: "Hofer", constructed: "4-10-1993", style: "Fantastic", addressnum: "17"},
+  {name: "Hofer", constructed: "04-10-1993", style: "Fantastic", addressnum: "17"},
   {name: "Stevenson", constructed: "26-12-1703", style: "Not as Fantastic", addressnum: "19"},
-  {name: "Steve", constructed: "1-1-1492", style: "Boring", addressnum: "46"},
+  {name: "Steve", constructed: "01-01-1492", style: "Boring", addressnum: "46"},
   {name: "Bill", constructed: "12-12-1872", style: "Intricate", addressnum: "20"},
-  {name: "Gatorade", constructed: "8-11-1912", style: "Good", addressnum: "11"},
-  {name: "Oreo", constructed: "5-2-1945", style: "Circular", addressnum: "18"},
-  {name: "Nabisco", constructed: "18-7-1940", style: "Alright", addressnum: "49"},
-  {name: "Nintendo", constructed: "15-9-1980", style: "Best", addressnum: "32"}
+  {name: "Gatorade", constructed: "08-11-1912", style: "Good", addressnum: "11"},
+  {name: "Oreo", constructed: "05-02-1945", style: "Circular", addressnum: "18"},
+  {name: "Nabisco", constructed: "18-07-1940", style: "Alright", addressnum: "49"},
+  {name: "Nintendo", constructed: "15-09-1980", style: "Best", addressnum: "32"}
 ];
 
 if (typeof module != "undefined" && module.exports)
