4caa3877c2fb3c1efd5bf1bffc64dbd1e2225d3e
Phillip <phv9495@saintjoe.edu>
2015-03-19 23:25:57 -0400

    Decided to delete the dataset that I was going to use in favor of reusing houses and renaming everything inside it.

---
+++ buildings.js
@@ -0,0 +1,13 @@
+var BUILDINGS = [
+  {name: "Hofer", constructed: 1993, style: "Fantastic", builders: "John and Suzan"},
+  {name: "Stevenson", constructed: 1703, style: "Not as Fantastic", builders: "Pinak and Glitchy"},
+  {name: "Steve", constructed: 1492, style: "Boring", builders: "Blue's Clues and Mail"},
+  {name: "Bill", constructed: 1872, style: "Intricate", builders: "Flesh and Bone"},
+  {name: "Gatorade", constructed: 1912, style: "Good", builders: "Water and Electrolytes"},
+  {name: "Oreo", constructed: 1945, style: "Circular", builders: "Cookie and Creme"},
+  {name: "Nabisco", constructed: 1940, style: "Alright", builders: "Oreos and Milk"},
+  {name: "Nintendo", constructed: 1980, style: "Best", builders: "Mario and Luigi"}
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = HOUSES;
