30bdd4132510b5c085dae8c28c51b85f4f89bcc2
Phillip <phv9495@saintjoe.edu>
2015-03-19 23:41:23 -0400

    Modify the date to have days and months as well. Format is day-month-year

--- buildings.js
+++ buildings.js
@@ -1,12 +1,12 @@
 var BUILDINGS = [
-  {name: "Hofer", constructed: 1993, style: "Fantastic", addressnum: "17"},
-  {name: "Stevenson", constructed: 1703, style: "Not as Fantastic", addressnum: "19"},
-  {name: "Steve", constructed: 1492, style: "Boring", addressnum: "46"},
-  {name: "Bill", constructed: 1872, style: "Intricate", addressnum: "20"},
-  {name: "Gatorade", constructed: 1912, style: "Good", addressnum: "11"},
-  {name: "Oreo", constructed: 1945, style: "Circular", addressnum: "18"},
-  {name: "Nabisco", constructed: 1940, style: "Alright", addressnum: "49"},
-  {name: "Nintendo", constructed: 1980, style: "Best", addressnum: "32"}
+  {name: "Hofer", constructed: "4-10-1993", style: "Fantastic", addressnum: "17"},
+  {name: "Stevenson", constructed: "26-12-1703", style: "Not as Fantastic", addressnum: "19"},
+  {name: "Steve", constructed: "1-1-1492", style: "Boring", addressnum: "46"},
+  {name: "Bill", constructed: "12-12-1872", style: "Intricate", addressnum: "20"},
+  {name: "Gatorade", constructed: "8-11-1912", style: "Good", addressnum: "11"},
+  {name: "Oreo", constructed: "5-2-1945", style: "Circular", addressnum: "18"},
+  {name: "Nabisco", constructed: "18-7-1940", style: "Alright", addressnum: "49"},
+  {name: "Nintendo", constructed: "15-9-1980", style: "Best", addressnum: "32"}
 ];
 
 if (typeof module != "undefined" && module.exports)
